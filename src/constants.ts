1d6d90ec25a7e6d0aa79f40e7b8f80f8ef07c1c7
Wlad Paiva <desenvolvedormg@gmail.com>
2022-08-10 12:42:34 -0300

    feat: added pronouns

--- src/constants.ts
+++ src/constants.ts
@@ -1,60 +1,74 @@
 const conjunctions = [
-  'e',
-  'como',
-  'ou',
-  'so',
-  'tambem',
-  'tanto',
-  'quanto',
-  'que',
-  'mas',
-  'tao',
-  'tamanho',
-  'quando',
-  'ainda',
-]
+  "e",
+  "como",
+  "ou",
+  "so",
+  "tambem",
+  "tanto",
+  "quanto",
+  "que",
+  "mas",
+  "tao",
+  "tamanho",
+  "quando",
+  "ainda",
+];
 
-const articles = ['o', 'os', 'a', 'as', 'um', 'uma', 'uns', 'umas']
+const articles = ["o", "os", "a", "as", "um", "uma", "uns", "umas"];
+
+const pronouns = ["seu", "sua"];
 
 const prepositions = [
-  'a',
-  'ao',
-  'aos',
-  'ante',
-  'apos',
-  'ate',
-  'com',
-  'contra',
-  'de',
-  'da',
-  'do',
-  'desde',
-  'em',
-  'no',
-  'nos',
-  'na',
-  'nas',
-  'entre',
-  'para',
-  'per',
-  'perante',
-  'por',
-  'sem',
-  'sob',
-  'sobre',
-  'tras',
-]
+  "a",
+  "ao",
+  "aos",
+  "ante",
+  "apos",
+  "ate",
+  "com",
+  "contra",
+  "de",
+  "da",
+  "do",
+  "desde",
+  "em",
+  "no",
+  "nos",
+  "na",
+  "nas",
+  "entre",
+  "para",
+  "per",
+  "perante",
+  "por",
+  "sem",
+  "sob",
+  "sobre",
+  "tras",
+  "vão",
+];
 
 export const lowerCase = new Set([
   ...conjunctions,
   ...articles,
+  ...pronouns,
   ...prepositions,
-])
+]);
 
 // special cases
-export const specials = ['BBQ', 'IPA', 'LN', 'EA', 'uni', 'gás', 'fatia']
+export const specials = [
+  "BBQ",
+  "IPA",
+  "LN",
+  "EA",
+  "uni",
+  "gás",
+  "fatia",
+  "preferência",
+  "tamanho",
+];
 
 // sometimes users just get it wrong
 export const fixes = {
-  '([a-z0-9])\\(': '$1 (', // add space before parenthesis
-} as {[key: string]: string}
+  "([a-z0-9])\\(": "$1 (", // add space before parenthesis
+} as { [key: string]: string };
